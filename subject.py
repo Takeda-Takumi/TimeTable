88637007a0ad93c73eca2ec48777b11a6b59fb7c
unknown <sulla6127.sura@gmail.com>
2022-02-12 23:38:49 +0900

    [add] class file (subject.py) wo tsuika
    kyoutuu no class file wo tsuika shimashita

---
+++ subject.py
@@ -0,0 +1,47 @@
+# -*- coding:utf-8 -*-
+# *** do not edit ***
+
+from datetime import datetime
+
+class Assignment:
+    def __init__(self):
+        self._name = ""
+        self._deadline = datetime.max
+
+    def set_name(self, name):
+        self._name = name
+
+    def get_name(self):
+        return self._name
+
+    def set_deadline(self, year, month, day, hour, minutes):
+        self.deadline = datetime(year, month, day, hour, minutes)
+
+    def get_deadline(self):
+        return self._deadline
+
+
+class Subject:
+    def __init__(self):
+        self._name = ""
+        self._assignments = [Assignment() for i in range(1)]
+        self._memo = ""
+
+    def add_asg(self):
+        self._assignments.append()
+
+    def del_asg(self):
+        # dareka kaitoite kudasai
+        return
+
+    def set_name(self, name):
+        self._name = name
+
+    def get_name(self):
+        return self._name
+    
+    def set_memo(self, memo):
+        self._memo = memo
+
+    def get_memo(self):
+        return self._memo
