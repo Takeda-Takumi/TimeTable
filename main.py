cb13291b693f55de93bf28df066168036a047457
Takeda-Takumi <takedatakumi6108@gmail.com>
2022-02-20 15:55:33 +0900

    [clean] main.pyの__init__メソッドの整理
    __init__にテストデータの入力やカレンダーの作成などがたくさん書いてあったため、可読性を上げるためにそれぞれの用途ごとにメソッドかしました。
    create_teimtable() : カレンダー本体を表示するメソッド
    input_test_data() : テストデータを入力するメソッド

--- main.py
+++ main.py
@@ -15,33 +15,6 @@ class Application(tk.Frame):
         self._detail_window = None # DetailWindow型の変数
         self._dw_is_open = False # _detail_windowがすでに開いてるかどうかの変数
 
-        # ラベルを作成、配置
-        labelMon = tk.Label(text="月", width = 14, height = 5)
-        labelMon.grid(column=1, row=0)
-        labelTue = tk.Label(text="火", width = 14, height = 5)
-        labelTue.grid(column=2, row=0)
-        labelWed = tk.Label(text="水", width = 14, height = 5)
-        labelWed.grid(column=3, row=0)
-        labelThu = tk.Label(text="木", width = 14, height = 5)
-        labelThu.grid(column=4, row=0)
-        labelFri = tk.Label(text="金", width=14, height=5)
-        labelFri.grid(column=5, row=0)
-        labelSat = tk.Label(text="土", fg="#0000F0", width=14, height=5)
-        labelSat.grid(column=6, row=0)
-
-        label = tk.Label(text="1", width = 14, height = 5)
-        label.grid(column=0, row=1)
-        label = tk.Label(text="2", width = 14, height = 5)
-        label.grid(column=0, row=2)
-        label = tk.Label(text="3", width = 14, height = 5)
-        label.grid(column=0, row=3)
-        label = tk.Label(text="4", width = 14, height = 5)
-        label.grid(column=0, row=4)
-        label = tk.Label(text="5", width = 14, height = 5)
-        label.grid(column=0, row=5)
-        label = tk.Label(text="6", width = 14, height = 5)
-        label.grid(column=0, row=6)
-
         # ウィジェットを作成
         self.widgets = [wg.Widget() for i in range(36)]
         for i in range(6):
@@ -51,30 +24,8 @@ class Application(tk.Frame):
                 # ウィジェットのボタンを配置
                 self.widgets[6*i+j].set_grid(j+1, i+1)
 
-        # テスト用
-        self.widgets[6]._subject.set_name("オペレーティングシステム技術")
-        self.widgets[6]._subject.add_asg()
-        self.widgets[6]._subject.set_asg_name(0, "レポート")
-        self.widgets[6]._subject.set_asg_deadline(0, 2022, 2, 20, 23, 55)
-        self.widgets[7]._subject.set_name("最適化")
-        self.widgets[12]._subject.set_name("ソフトウェア技術")
-        self.widgets[12]._subject.add_asg()
-        self.widgets[12]._subject.set_asg_name(0, "小テスト")
-        self.widgets[12]._subject.set_asg_deadline(0, 2022, 2, 14, 0, 0)
-        self.widgets[13]._subject.set_name("数理情報学3")
-        self.widgets[15]._subject.set_name("科学技術英語")
-        self.widgets[18]._subject.set_name("大規模・高速計算")
-        self.widgets[18]._subject.add_asg()
-        self.widgets[18]._subject.set_asg_name(0, "レポート")
-        self.widgets[18]._subject.set_asg_deadline(0, 2022, 2, 14, 23, 55)
-        self.widgets[19]._subject.set_name("開発系プログラミング演習")
-        self.widgets[19]._subject.add_asg()
-        self.widgets[19]._subject.set_asg_name(0, "週課題")
-        self.widgets[19]._subject.set_asg_deadline(0, 2022, 2, 15, 15, 0)
-        self.widgets[20]._subject.set_name("マルチメディア情報処理")
-        self.widgets[20]._subject.add_asg()
-        self.widgets[20]._subject.set_asg_name(0, "週課題")
-        self.widgets[20]._subject.set_asg_deadline(0, 2022, 2, 16, 15, 0)
+        self.create_timetable()
+        self.input_test_data()
 
 
     # ---ボタンを押せはするが、ウィンドウは１つしか開かなくする方法---
@@ -133,6 +84,62 @@ class Application(tk.Frame):
     def update(self):
         self.change_text_and_color()
         self.after(60000, self.update) # 60000ms後にもう一度実行
+
+    # テスト用
+    def input_test_data(self):
+        self.widgets[6]._subject.set_name("オペレーティングシステム技術")
+        self.widgets[6]._subject.add_asg()
+        self.widgets[6]._subject.set_asg_name(0, "レポート")
+        self.widgets[6]._subject.set_asg_deadline(0, 2022, 2, 20, 23, 55)
+        self.widgets[7]._subject.set_name("最適化")
+        self.widgets[12]._subject.set_name("ソフトウェア技術")
+        self.widgets[12]._subject.add_asg()
+        self.widgets[12]._subject.set_asg_name(0, "小テスト")
+        self.widgets[12]._subject.set_asg_deadline(0, 2022, 2, 14, 0, 0)
+        self.widgets[13]._subject.set_name("数理情報学3")
+        self.widgets[15]._subject.set_name("科学技術英語")
+        self.widgets[18]._subject.set_name("大規模・高速計算")
+        self.widgets[18]._subject.add_asg()
+        self.widgets[18]._subject.set_asg_name(0, "レポート")
+        self.widgets[18]._subject.set_asg_deadline(0, 2022, 2, 14, 23, 55)
+        self.widgets[19]._subject.set_name("開発系プログラミング演習")
+        self.widgets[19]._subject.add_asg()
+        self.widgets[19]._subject.set_asg_name(0, "週課題")
+        self.widgets[19]._subject.set_asg_deadline(0, 2022, 2, 15, 15, 0)
+        self.widgets[20]._subject.set_name("マルチメディア情報処理")
+        self.widgets[20]._subject.add_asg()
+        self.widgets[20]._subject.set_asg_name(0, "週課題")
+        self.widgets[20]._subject.set_asg_deadline(0, 2022, 2, 16, 15, 0)
+
+    # ラベルを作成、配置
+    def create_timetable(self):
+        labelMon = tk.Label(text="月", width = 14, height = 5)
+        labelMon.grid(column=1, row=0)
+        labelTue = tk.Label(text="火", width = 14, height = 5)
+        labelTue.grid(column=2, row=0)
+        labelWed = tk.Label(text="水", width = 14, height = 5)
+        labelWed.grid(column=3, row=0)
+        labelThu = tk.Label(text="木", width = 14, height = 5)
+        labelThu.grid(column=4, row=0)
+        labelFri = tk.Label(text="金", width=14, height=5)
+        labelFri.grid(column=5, row=0)
+        labelSat = tk.Label(text="土", fg="#0000F0", width=14, height=5)
+        labelSat.grid(column=6, row=0)
+
+        label = tk.Label(text="1", width = 14, height = 5)
+        label.grid(column=0, row=1)
+        label = tk.Label(text="2", width = 14, height = 5)
+        label.grid(column=0, row=2)
+        label = tk.Label(text="3", width = 14, height = 5)
+        label.grid(column=0, row=3)
+        label = tk.Label(text="4", width = 14, height = 5)
+        label.grid(column=0, row=4)
+        label = tk.Label(text="5", width = 14, height = 5)
+        label.grid(column=0, row=5)
+        label = tk.Label(text="6", width = 14, height = 5)
+        label.grid(column=0, row=6)
+
+
 
 
 ##------------------
